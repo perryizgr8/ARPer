dfb4b36df5a5de87c5e5b3effd832057e75d39d3
Parikshit Misra <perryizgr8@live.in>
2016-01-02 23:30:33 +0530

    UI done, now need to make thread and send packets

--- ARPer/MainWindow.xaml.cs
+++ ARPer/MainWindow.xaml.cs
@@ -28,7 +28,58 @@ namespace ARPer
         private void attackMe_GotFocus(object sender, RoutedEventArgs e)
         {
             attackMe.Foreground = Brushes.Black;
-            attackMe.Text = "";
+            if (attackMe.Text == "IP to attack")
+            {
+                attackMe.Text = "";
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            attackMe.Focus();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isIpValid(attackMe.Text))
+            {
+                sendArp(attackMe.Text); 
+            }
+            else
+            {
+                attackMe.Focus();
+                attackMe.SelectAll();
+            }
+        }
+
+        private bool isIpValid(string text)
+        {
+            //throw new NotImplementedException();
+            var quads = text.Split('.');
+
+            // if we do not have 4 quads, return false
+            if (!(quads.Length == 4)) return false;
+
+            // for each quad
+            foreach (var quad in quads)
+            {
+                int q;
+                // if parse fails 
+                // or length of parsed int != length of quad string (i.e.; '1' vs '001')
+                // or parsed int < 0
+                // or parsed int > 255
+                // return false
+                if (!Int32.TryParse(quad, out q)
+                    || !q.ToString().Length.Equals(quad.Length)
+                    || q < 0
+                    || q > 255) { return false; }
+            }
+            return true;
+        }
+
+        private void sendArp(string text)
+        {
+            throw new NotImplementedException();
         }
     }
 }
